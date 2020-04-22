5fd4d44115d1dbd9f0fe056dd2466e78a02d726c
Will Smith <wds68@cornell.edu>
2020-04-22 02:04:02 -0400

    minor cleanup

--- glue.cpp
+++ glue.cpp
@@ -20,56 +20,6 @@ extern "C" {
         src->copyTo(*dst);
     }
 
-    // cv::Mat *mat_of_bigarray(bigarray *arr) {
-    //     // The last dimension is for different channels.
-    //     // OpenCV treats this separately from other dimensions,
-    //     // but OCaml's Bigarray treats them just like other dimensions.
-    //     int ndims = arr->num_dims - 1;
-    //     int *dims = new int[ndims];
-    //     for (int i = 0; i < ndims; i++) {
-    //         dims[i] = arr->dim[i];
-    //     }
-    //     int channels = arr->dim[ndims];
-    //     int type = CV_MAKETYPE(CV_8U, channels);
-    //     void *data = arr->data;
-
-    //     return new cv::Mat(ndims, dims, type, data);
-    // }
-
-    // void copy_mat_to_bigarray(cv::Mat *mat, bigarray *arr) {
-    //     arr->flags = CAML_BA_EXTERNAL + CAML_BA_C_LAYOUT + CAML_BA_UINT8;
-    //     arr->data = mat->data;
-
-    //     if (mat->size.dims() + 1 > arr->num_dims) {
-    //         // Uh-oh TODO throw exception
-    //         // We can't re-allocate the bigarray so we're kind of screwed
-    //     }
-    //     for (int i = 0; i < mat->size.dims(); i++) {
-    //         arr->dim[i] = mat->size[i];
-    //     }
-    //     arr->num_dims = mat->size.dims() + 1;
-    //     arr->dim[mat->size.dims()] = mat->channels();
-    // }
-
-    // value bigarray_of_mat(cv::Mat *mat) {
-    //     //bigarray *arr = new bigarray;
-    //     //copy_mat_to_bigarray(mat, arr);
-
-    //     //intnat flags = CAML_BA_EXTERNAL + CAML_BA_C_LAYOUT + CAML_BA_UINT8;
-    //     int flags = CAML_BA_C_LAYOUT | CAML_BA_UINT8;
-    //     void *data = mat->data;
-    //     intnat num_dims = mat->size.dims() + 1;
-    //     intnat *dim = new intnat[num_dims];
-    //     for (int i = 0; i < mat->size.dims(); i++) {
-    //         dim[i] = mat->size[i];
-    //     }
-    //     dim[mat->size.dims()] = mat->channels();
-
-    //     printf("dims: %i\n", (int) num_dims);
-
-    //     return caml_ba_alloc(flags, num_dims, data, dim);
-    // }
-
     int mat_num_dims(cv::Mat *mat) {
         return mat->size.dims() + 1;
     }
